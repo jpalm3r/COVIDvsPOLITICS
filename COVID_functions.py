4780710fa65485d36b8bdf2c9d6ae0aebf899041
Jaume <rp.jaume@gmail.com>
2021-11-29 12:09:54 +0100

    Formatting website

--- COVID_functions.py
+++ COVID_functions.py
@@ -6,35 +6,35 @@ import numpy as np
 
 
 def CountWord(title, word):
-    
-    from collections import Counter 
-    
+
+    from collections import Counter
+
     tokens = tokenizer(title)
-    
+
     c = Counter(tokens)
-    
+
     if word in c.keys():
         return c[word]
     else:
         return 0
-    
+
 def COVIDwordIndex(string, topics):
 
     return sum([int(x in string)/len(topics) for x in topics])
 
 
 def RateChange(word, d2019, d2020):
-    
+
     n19 = d2019.comments.apply(lambda x: x.count(word)).sum()/d2019.shape[0]
     n20 = d2020.comments.apply(lambda x: x.count(word)).sum()/d2020.shape[0]
-    
-    return n20/(n20 + n19)    
-    
+
+    return n20/(n20 + n19)
+
 def HasCOVIDword(tokens, word):
     return(word in tokens)
-   
+
     #print(w_i, rc_i, rc_j)
-    
+
 
 def CovidRelated(title_string):
     title_string = title_string.lower()
@@ -52,7 +52,7 @@ def ReadSubreddit(sr, agg_step = 'week', year = 2020):
     data0_folder = parent_folder + 'data/'
     comment_folder = data0_folder + 'posts/comments/'
     data_folder = comment_folder + sr + '/'
-    
+
     if year != 2020:
         data_folder = data_folder = comment_folder + sr + '/' + str(year) + '/'
 
@@ -97,7 +97,7 @@ def ReadSubreddit(sr, agg_step = 'week', year = 2020):
     metadat0['week'] = metadat0['TimeStamp'].dt.week
     metadat0['day'] = metadat0['TimeStamp'].dt.dayofyear
     metadat0['month'] = metadat0['TimeStamp'].dt.month
-    
+
     metadat0['period'] = metadat0[agg_step]
 
     metadat0['comments'] = comment_list
@@ -105,50 +105,50 @@ def ReadSubreddit(sr, agg_step = 'week', year = 2020):
     dat1 = metadat0[metadat0['TimeStamp'].dt.year.isin([year])]
     dat1['hour'] = dat1['TimeStamp'].dt.hour
     dat1['hour_retrieved'] = dat1['TimeStamp_retrieved'].dt.hour
-    
+
     return(dat1)
 
 
 
 
 def tokenizer(text):
-       
+
     #Import visualization tools for LDA models
     from nltk.stem.snowball import SnowballStemmer
     import string
-    
+
     '''
     -covert everything to lowercase
     -remove punctuations
     -remove stopwords
     -stemmer
     '''
-    
-    # Using alternative stopwords    
+
+    # Using alternative stopwords
     stopwords_alt = []
     with open('/home/jpre/Documents/DTU/COVIDpolitics2021/COVIDvsPOLITICS/stopword_list.txt', 'r') as file:
         stopwords_alt = file.read().split('\n')
-        
+
     stopwords_alt = stopwords_alt + ['time', 'state', '10','–',
                                      '1','2','3','2020','amid','de','en','day',
                                      'people','case','cases','florida','live','update','updating',
                                      'covid19', 'coronavirus','covid','virus']
-       
-    
+
+
     #All characters in this string will be converted to lowercase
     text = text.lower()
-    
+
     #Removing sentence punctuations
     for punctuation_mark in string.punctuation:
         text = text.replace(punctuation_mark,'')
-    
+
     #Creating our list of tokens
     list_of_tokens = text.split(' ')
-    #Creating our cleaned tokens list 
+    #Creating our cleaned tokens list
     cleaned_tokens = []
     #Let us use a stemmer
     stemmer = SnowballStemmer(language = 'english')
-    
+
     #Removing Stop Words in our list of tokens and any tokens that happens to be empty strings
     for token in list_of_tokens:
         if (not token in stopwords_alt) and (token != ''):
@@ -156,13 +156,13 @@ def tokenizer(text):
             token_stemmed = stemmer.stem(token)
             #appending our finalized cleaned token
             cleaned_tokens.append(token_stemmed)
-    
+
     return cleaned_tokens
 
 
 def NumWords(string):
     global tokenizer
-    
+
     return(len(tokenizer(string)))
 
 
@@ -178,63 +178,63 @@ def empty_canvas(p):
     p.outline_line_color = None
     p.background_fill_color = None
     p.border_fill_color = None
-    
+
     return(p)
 
 
 
 
 def AddText(p_j, xy, word, fs, ci):
-    
+
 
     from bokeh.models import ColumnDataSource
     from bokeh.models import Text
-    
+
     # Adding text in the middle
     glyph_text = Text(x='x', y='y', text="text", text_baseline = 'middle', text_align = 'center',
               angle=0., text_font_size = fs, text_color=ci, text_alpha = 0.6,
               text_font_style = 'bold')
-    p_j.add_glyph(ColumnDataSource(dict(x=[xy[0]], y=[xy[1]], text=[word])),glyph_text) 
-    
+    p_j.add_glyph(ColumnDataSource(dict(x=[xy[0]], y=[xy[1]], text=[word])),glyph_text)
+
     return(p_j)
 
 
 def CreateDfRelevant(word_i, Nr, dc19, datest, impact_metric = 'num_comments'):
-       
+
     def declare_global_word(w):
         global word_j
         word_j = w
-    
+
     def HasCOVIDword(tokens):
         global word_j
         return(word_j in tokens)
-    
+
     declare_global_word(word_i)
 
-    word_j = word_i    
+    word_j = word_i
     titles_withword = dc19[['period',impact_metric,'title']].loc[dc19.tokens.apply(HasCOVIDword) == True]
     idx_rel = titles_withword.groupby(['period'])[impact_metric].transform(max) == titles_withword[impact_metric]
     titles_withword = titles_withword[['period','title',impact_metric]][idx_rel]
     titles_withword = titles_withword.set_index('period')
-    
+
     titles_withword = titles_withword.dropna()
-    
+
     i_relevant = titles_withword.num_comments.nlargest(Nr).index
     df_relevant = datest.join(titles_withword, how='outer').reset_index().iloc[i_relevant]
-    
+
     return(df_relevant)
 
 def CreateCalendarPlot(word_i, relevant_days, relevant_titles, width = 320, ms = 6, col = 'blue'):
-    
+
     import numpy as np
     import datetime
     import pandas as pd
-    
+
     from bokeh.models import ColumnDataSource, HoverTool
     from bokeh.plotting import figure
-    
+
     TOOLTIPS_title = """
-        <div>
+        <div style="background-color: white; cursor: pointer; align-content: left; text-align: left;">
             <div>
                 <span style="font-size: 18px; font-weight: bold;">@date</span>
             </div>
@@ -269,12 +269,12 @@ def CreateCalendarPlot(word_i, relevant_days, relevant_titles, width = 320, ms =
     x_calendar_flat = list(np.array(x_calendar_flat)[relevant_days])
     y_calendar_flat = list(np.array(y_calendar_flat)[relevant_days])
     color_points = list(np.array(color_points)[relevant_days])
-    
+
     relevant_dates = []
     for rd in relevant_days:
         x = datetime.datetime.strptime(' '.join(['2020', str(rd)]), '%Y %j')
         relevant_dates.append(x.strftime('%d/%m/%Y'))
-    
+
     df_calendar = pd.DataFrame({'x':x_calendar_flat,
                                 'y':y_calendar_flat,
                                 'color':color_points,
@@ -291,7 +291,7 @@ def CreateCalendarPlot(word_i, relevant_days, relevant_titles, width = 320, ms =
                           size = ms)
     # Adding hover tool
     hover1 = HoverTool(tooltips=TOOLTIPS_title,renderers = [r_post], mode = 'mouse')
-    p_cal.tools.append(hover1)   
+    p_cal.tools.append(hover1)
 
     # Adding ini and end of year
     p_cal.triangle(0, calendar_rows-1, line_color = None, fill_color = 'black', size = ms, angle = -np.pi/2)
@@ -308,9 +308,9 @@ def CreateCalendarPlot(word_i, relevant_days, relevant_titles, width = 320, ms =
 
     p_cal.xgrid.ticker = list([x-0.5 for x in range(calendar_cols)])
     p_cal.ygrid.ticker = list([y-0.5 for y in range(calendar_cols)])
-    
+
     p_cal.background_fill_color = "seashell"
-    
+
     p_cal.outline_line_color = None
     p_cal.border_fill_color = None
 
@@ -321,14 +321,14 @@ def CreateCalendarPlot(word_i, relevant_days, relevant_titles, width = 320, ms =
 def flatten(l):
     return([item for sublist in l for item in sublist])
 
-def CreateRankingPlot(dat5, controversy_topics, ndays = 14, col1 = 'skyblue', col2 = 'orangered', col3 = 'hotpink'):
-    
+def CreateRankingPlot(dat5, controversy_topics, ndays = 14, col1 = 'skyblue', col2 = 'orangered', col3 = 'hotpink', plot_dimensions = (600,200)):
+
     from scipy import interpolate
     import pandas as pd
     import numpy as np
     from bokeh.models import ColumnDataSource, TapTool, MultiLine, Text
     from bokeh.plotting import figure
-    
+
     def add_widths(x, y, width=0.1):
         """ Adds flat parts to widths """
         new_x = []
@@ -337,10 +337,10 @@ def CreateRankingPlot(dat5, controversy_topics, ndays = 14, col1 = 'skyblue', co
             new_x += [i-width, i, i+width]
             new_y += [j, j, j]
         return new_x, new_y
-    
+
 
     TOOLTIPS_word = """
-        <div>
+        <div style="background-color: white; cursor: pointer; align-content: left; text-align: left;>
             <div>
                 <span style="font-size: 18px; font-weight: bold; color: #820872;">@word</span>
             </div>
@@ -373,8 +373,8 @@ def CreateRankingPlot(dat5, controversy_topics, ndays = 14, col1 = 'skyblue', co
 
     n_periods = len(dat5_max.period.unique())
     dict_multi = {'x' : [], 'y' : [], 'word' : []}
-    
-    # Words during the week 
+
+    # Words during the week
     start_week = 1
     words_0 = dat5_max[dat5_max.period == start_week].variable.tolist()[::-1]
     # The curves are interpolated stepsize times
@@ -397,12 +397,15 @@ def CreateRankingPlot(dat5, controversy_topics, ndays = 14, col1 = 'skyblue', co
 
     source = ColumnDataSource(dict_multi)
 
-    p6 = figure(plot_width = 1200, plot_height = 500, tools = '', y_range = (0,19.5), x_range = (1, 26),
+    pw = plot_dimensions[0]
+    ph = plot_dimensions[1]
+
+    p6 = figure(plot_width = pw, plot_height = ph, tools = '', y_range = (0,19.5), x_range = (1, 26),
                 tooltips=TOOLTIPS_word, title = '(click to fix the line on the screen!)')
     ml = p6.multi_line(xs='x', ys='y',line_width=1, line_color=col1,line_alpha=0.3,
                  hover_line_color=col2, hover_line_alpha=1.0,
                  source=source)
-    
+
     ml.hover_glyph.line_width=2.5
 #    ml.tap_glyph.line_color='red'
 
@@ -413,11 +416,11 @@ def CreateRankingPlot(dat5, controversy_topics, ndays = 14, col1 = 'skyblue', co
     p6.add_tools(tap_line)
 
     p6 = empty_canvas(p6)
-    
+
     p6.yaxis.visible = True
     yaxis_tickpos = list(range(1,len(controversy_topics)+1))
     p6.yaxis.ticker = yaxis_tickpos
-    
+
     p6.yaxis.major_label_overrides = {(pos+1):lab for pos,lab in enumerate(words_0)}
     p6.yaxis.major_label_text_font_size = '16pt'
     p6.yaxis.major_label_text_color = 'black'
@@ -425,11 +428,11 @@ def CreateRankingPlot(dat5, controversy_topics, ndays = 14, col1 = 'skyblue', co
     p6.yaxis.axis_line_color = None
 
     p6.title.text_font_size = '16pt'
-    
-    
+
+
     # Adding text
     '''
-    text1 = Text(x='x', y='y', text="text", text_font_size = '16pt', y_offset = 0, x_offset = 0, 
+    text1 = Text(x='x', y='y', text="text", text_font_size = '16pt', y_offset = 0, x_offset = 0,
                   text_color = 'blue', text_baseline = 'middle', text_align = 'right')
 
     p6.add_glyph(ColumnDataSource(dict(x=[0.5 for _ in words_0],
@@ -437,7 +440,7 @@ def CreateRankingPlot(dat5, controversy_topics, ndays = 14, col1 = 'skyblue', co
                                        text=words_0)), text1)
 
     '''
-    return(p6,dict_multi)
+    return(p6, dict_multi)
 
 
 def CreateMetricOneWord(w_i, y_rep, y_dem,sizedot = 9, sizefont = '28pt', lgnd = False, plotax = False, plotsize = (350,350), Colors = ('goldenrod','#820872','black')):
@@ -470,7 +473,7 @@ def CreateMetricOneWord(w_i, y_rep, y_dem,sizedot = 9, sizefont = '28pt', lgnd =
 	neutrality_index =  R2, np.exp(R2)*np.tanh(-(slope-1))
 
 	p_1 = figure(plot_width= plotsize[0], plot_height=plotsize[1],
-		     title = '', 
+		     title = '',
 		     x_range=(minxy,maxxy),y_range = (minxy,maxxy), tools = '')
 
 	center_xy = (maxxy+minxy)/2
@@ -528,7 +531,7 @@ def CreateMetricOneWord(w_i, y_rep, y_dem,sizedot = 9, sizefont = '28pt', lgnd =
 
 
 def CountWordsInDF(df, controversy_topics):
-    
+
     try:
         cpus = multiprocessing.cpu_count()
     except NotImplementedError:
@@ -550,29 +553,29 @@ def CountWordsInDF(df, controversy_topics):
     matrix_df.columns = controversy_topics
 
     df = pd.concat([df.reset_index(), matrix_df.reset_index()], axis=1)
-    
+
     return(df)
 
 
 # def GenerateTimelinePLot(i, indx, titles_withword, sizeplot = (320,80), TOOLS = 'box_zoom,reset'):
-    
+
 #     global datest, dc19, word_i
-    
+
 #     impact_metric = 'num_comments'
-    
+
 #     smooth_i = '_'.join([i,'ks'])
-    
+
 #     max_freq = 1.5*max(datest[i])
 #     min_freq = -0.5*max_freq
 #     med_freq = (max_freq + min_freq)/2
-    
+
 #     min_x = -5
 #     max_x = 370
-    
+
 #     p = figure(plot_width= sizeplot[0], plot_height=sizeplot[1], title = None,
 #                x_range=(min_x,max_x), y_range = (min_freq,max_freq), tools = TOOLS)
-    
-   
+
+
 #     # Selecting the Nr relevant posts
 #     Nr = 19
 #     bsl = med_freq
@@ -580,12 +583,12 @@ def CountWordsInDF(df, controversy_topics):
 #     df_relevant = datest.join(titles_withword, how='outer').reset_index().iloc[i_relevant]
 #     df_relevant['baseline'] = bsl
 #     source_posts = ColumnDataSource(data= df_relevant)
-    
+
 #     glyph_text = Text(x='x', y='y', text="text", text_baseline = 'middle', text_align = 'center',
 #                       angle=0., text_font_size = '42pt', text_color=c3, text_alpha = 0.6,
 #                       text_font_style = 'bold')
-    
-    
+
+
 #     # Vertical line
 #     event_lines = []
 #     # 7 / 7-Jan / Chinese authorities report coronavirus
@@ -593,7 +596,7 @@ def CountWordsInDF(df, controversy_topics):
 #     # 24 / 24-Jan / First reported COVID case in the Europe
 #     # 73 / 13-Mar / Major lockdown in Europe
 #     # 106 / 15-Apr / Europe surpasses 1mil death
-#     # 285 / 11-Oct / Trump test positive    
+#     # 285 / 11-Oct / Trump test positive
 #     # 308 / 13-Nov / US elections
 #     # 343 / 8-Dec / First person gets vaccinated
 #     Events = [7,20,24,73,106,285,308,343]
@@ -602,25 +605,25 @@ def CountWordsInDF(df, controversy_topics):
 #                                 line_width=0.5, line_dash = 'dotted', level = 'underlay'))
 #         p.renderers.extend(event_lines)
 
-  
+
 #     # Timeline
 #     p.line(x=[1,365],y=[bsl,bsl], line_color = c3, line_width = 2)
 #     p.scatter(x=[1,365],y=[bsl,bsl], color = c3, line_color = c3, line_width = 2, angle =0)
-     
+
 #     # Relevant posts
 #     r = p.scatter(y='baseline', x='index', color=c1, source=source_posts,
 #                   alpha = 1, size=16, marker = 'diamond', line_color = c3, line_width = 2)
-    
+
 #     # curtain rectangle
 #     r_rect = p.rect(x=-100, y = -100, height = 1000, width = 1000, fill_color = 'white', fill_alpha = 1)
-        
-#     r_text = p.add_glyph(ColumnDataSource(dict(x=[30*6], y=[med_freq], text=[i])), glyph_text) 
-    
-    
+
+#     r_text = p.add_glyph(ColumnDataSource(dict(x=[30*6], y=[med_freq], text=[i])), glyph_text)
+
+
 #     hover1 = HoverTool(tooltips=TOOLTIPS_title,renderers = [r], mode = 'vline')
 #     hover2 = HoverTool(tooltips = None, renderers = [r_text], mode = 'mouse')
 #     hover3 = HoverTool(tooltips = None,renderers = [r_rect], mode = 'mouse')
-    
+
 #     p.tools.append(hover1)
 #     p.tools.append(hover2)
 #     p.tools.append(hover3)
@@ -632,7 +635,7 @@ def CountWordsInDF(df, controversy_topics):
 #     # I add the selection glyph because I want to fix a particular line for comparing
 #     r_text.hover_glyph = selected_text
 #     r_rect.hover_glyph = selected_rect
-    
+
 #     # Adjusting plot parameters
 #     #p.x_range.range_padding = 0.05
 #     p.xaxis.axis_label = ""
@@ -643,10 +646,10 @@ def CountWordsInDF(df, controversy_topics):
 #     p.yaxis.ticker = [0,max_freq]
 #     p.grid.visible = False
 #     p.background_fill_alpha = 1
-    
+
 #     p.outline_line_color = None
 #     p.border_fill_color = None
-    
+
 #     p.background_fill_color = "seashell"
-        
+
 #     return p
